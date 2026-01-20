11c0595cc40c7c0d8068add9e1b419f239047f76
khaalidshittu9-star <khaalidshittu9@gmail.com>
2026-01-20 06:08:04 -0800

    Add files via upload

---
+++ first_int.py
@@ -0,0 +1,10 @@
+my_name = "Hemark"
+var = "89
+print(var)
+print(my_name)
+print
+print(type()my_name)
+var =  int(var)
+question = "what is your name? "
+answer = "My name is\ {my_name}"
+# if num 2
